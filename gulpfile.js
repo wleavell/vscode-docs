95d366158aa3e89ebeefbf242ff7dddddafc9c13
Pine Wu <octref@gmail.com>
2017-12-01 10:00:51 -0800

    Trigger build

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp')
 const $ = require('shelljs')
 
+console.log(process.env['token'])
 const TOKEN = process.env['token']
 if (!TOKEN) {
   $.echo('This script clones vscode-website and requires access token')
