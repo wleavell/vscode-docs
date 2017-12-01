deb8e8d328f44711a3edc2ee14d629dfdde916b1
Pine Wu <octref@gmail.com>
2017-11-30 16:41:40 -0800

    Sync build

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+const gulp = require('gulp')
+const $ = require('shelljs')
+
+const TOKEN = process.env['token']
+if (!TOKEN) {
+  $.echo('This script clones vscode-website and requires access token')
+  $.exit(1)
+}
+
+const URL = `https://${TOKEN}@github.com/microsoft/vscode-website`
+
+gulp.task('clone-repo', done => {
+  if (!$.which('git')) {
+    $.echo('This command requires git')
+    $.exit(1)
+    done()
+  }
+
+  $.mkdir('_build')
+  $.cd('_build')
+  $.exec(`git clone --depth=1 --branch=sync-build ${URL}`)
+  $.cd('vscode-website')
+  $.exec(`token=${TOKEN} ./scripts/setup.sh`)
+})
